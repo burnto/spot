burnto/spot

commit 40d70aa44f9cdb73abd0016ac48c2505809559a2

Wait for the process to complete before respawning

spot.go
@@ -77,6 +77,10 @@ func main() {
 				if err != nil {
 					log.Fatalln(err)
 				}
+				err = cmd.Process.Wait()
+				if err != nil {
+					log.Fatalln(err)
+				}
 			}
 		case err := <-watcher.Error:
 			log.Println("error", err)

commit 563f6fc62d79edf3de5b402a00d49b095799358f

Fix compilation error introduced in 40d70aa44f9cdb73abd0016ac48c2505809559a2

spot.go
@@ -77,7 +77,7 @@ func main() {
 				if err != nil {
 					log.Fatalln(err)
 				}
-				err = cmd.Process.Wait()
+				_, err = cmd.Process.Wait()
 				if err != nil {
 					log.Fatalln(err)
 				}